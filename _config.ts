badc69c4d9b7cc34c3612143a0493e221a208628
Oscar Otero <oom@oscarotero.com>
2024-09-25 00:30:06 +0200

    first commit

---
+++ _config.ts
@@ -0,0 +1,12 @@
+import lume from "lume/mod.ts";
+import lighningcss from "lume/plugins/lightningcss.ts";
+import favicon from "lume/plugins/favicon.ts";
+
+const site = lume({
+  location: new URL("https://css.gal/"),
+});
+site.use(lighningcss());
+site.use(favicon());
+site.copy("assets");
+
+export default site;
