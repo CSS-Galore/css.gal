894c1552ed6ba629eecd1595a437e99b58dc2954
Oscar Otero <oom@oscarotero.com>
2024-09-25 12:55:29 +0200

    added vscode plugin

--- _config.ts
+++ _config.ts
@@ -1,6 +1,8 @@
 import lume from "lume/mod.ts";
 import lighningcss from "lume/plugins/lightningcss.ts";
 import favicon from "lume/plugins/favicon.ts";
+import picture from "lume/plugins/picture.ts";
+import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
   location: new URL("https://css.gal/"),
@@ -8,5 +10,7 @@ const site = lume({
 site.use(lighningcss());
 site.use(favicon());
 site.copy("assets");
+site.use(picture());
+site.use(transformImages());
 
 export default site;
