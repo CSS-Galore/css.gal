9e146fc8c385a39989574672a927b0e38a26b00b
Oscar Otero <oom@oscarotero.com>
2024-09-26 17:42:32 +0200

    changes

--- _config.ts
+++ _config.ts
@@ -1,8 +1,6 @@
 import lume from "lume/mod.ts";
 import lighningcss from "lume/plugins/lightningcss.ts";
 import favicon from "lume/plugins/favicon.ts";
-import picture from "lume/plugins/picture.ts";
-import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
   location: new URL("https://css.gal/"),
@@ -10,8 +8,6 @@ const site = lume({
 site.use(lighningcss());
 site.use(favicon());
 site.copy("assets");
-site.copy([".js"]);
-site.use(picture());
-site.use(transformImages());
+site.copy([".js", ".mp4"]);
 
 export default site;
